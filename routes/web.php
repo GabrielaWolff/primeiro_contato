956c585df5151b1dca922c135629794b9d2e8e5c
“Gabriela <gabriela_w@dotcubed.io>
2022-03-31 14:59:40 +0000

    delete old files

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,12 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+ Route::get('/users',[UserController::class,'index'])->name('users.index');
+ Route::get('/users/{id}',[UserController::class,'show'])->name('users.show');
+
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('index', [PostController::class, 'index']);
-Route::get('show_post', [PostController::class, 'show']);
-Route::get('create_post', [PostController::class, 'create']);
-Route::post('store_post', [PostController::class, 'store']);
-Route::get('edit_post', [PostController::class, 'edit']);
-Route::put('update_post', [PostController::class, 'update']);
-Route::delete('delete_post', [PostController::class, 'delete']);
