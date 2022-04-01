faae6c24fd81b7f09903ee8da8b7d0d45e6cfcf0
“Gabriela <gabriela_w@dotcubed.io>
2022-04-01 15:20:42 +0000

    create foreach Listagem de Usuarios

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,17 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function index()
     {
-        return view('users.index');
+        $users = User::get();
+
+        return view('users.index',compact('users'));
     }
 
     public function show($id)
     {
+        $user = User::where('id',$id)->first();
+        dd($user);
         dd('users.show',$id);
     }
 
